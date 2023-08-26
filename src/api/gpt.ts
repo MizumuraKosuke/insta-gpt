24932cd89d8afcb42bfff490e86796d769b000c6
Kosuke Mizumura <mizumura.it@gmail.com>
2023-08-26 18:19:02 +0900

    fixed gpt4

--- src/api/gpt.ts
+++ src/api/gpt.ts
@@ -8,7 +8,7 @@ import { post } from '@/utils/fetch'
 
 export const sendMessageGPT = async (content: string) => {
   const options: Partial<CreateChatCompletionRequest> = {
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4',
     messages: [{ role: 'user', content }],
   }
   const result = await post<CreateChatCompletionResponse>(
