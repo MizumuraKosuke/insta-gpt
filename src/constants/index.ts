392f2633c1f25ca313a28804bf1869214fa5f671
Kosuke Mizumura <mizumura.it@gmail.com>
2024-02-19 05:07:13 +0900

    up path

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const OPENAI_HOST = 'https://api.openai.com/v1'
-export const OPENAI_API_KEY = ''
+export const OPENAI_API_KEY = process.env.EXPO_PUBLIC_OPENAI_API_KEY
 
 export const SWR_KEY = {
   TOAST: '/hooks/toast',
