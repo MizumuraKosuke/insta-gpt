b08d2d39bf0f8d31b459645709986ae5ad4a6d12
Kosuke Mizumura <mizumura.it@gmail.com>
2024-02-19 04:45:39 +0900

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const OPENAI_HOST = 'https://api.openai.com/v1'
-export const OPENAI_API_KEY = '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'
+export const OPENAI_API_KEY = ''
 
 export const SWR_KEY = {
   TOAST: '/hooks/toast',
